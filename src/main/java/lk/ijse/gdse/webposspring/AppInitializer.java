172a32235db12f3e47572222480d858ae70602fd
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-04 15:53:07 +0530

    application start method updated

---
+++ src/main/java/lk/ijse/gdse/webposspring/AppInitializer.java
@@ -0,0 +1,34 @@
+package lk.ijse.gdse.webposspring;
+
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import lk.ijse.gdse.webposspring.config.WebAppConfig;
+import lk.ijse.gdse.webposspring.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Prabodha Thathsarani
+ * @date 10/4/24
+ * @project webpos-spring
+ **/
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(new MultipartConfigElement("/tmp"));
+    }
+}
